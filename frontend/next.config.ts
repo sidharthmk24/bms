699680d28a42759182141e25411f6c23fb01f13e
Sidharth mk <sidharthmk24@gmail.com>
2026-08-25 14:46:15 +0530

    next config edited

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,11 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['typeorm', 'mysql2', 'bcrypt', 'typeorm-naming-strategies', 'reflect-metadata'],
   distDir: process.env.TEST_MODE === '1' ? '.next-test' : '.next',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
   },
 };
 
